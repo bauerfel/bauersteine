677f8ba1b34d9040872bcd990440e965dbc739f5
bauerfel <bauerfel@students.zhaw.ch>
2024-04-20 12:35:49 +0200

    Initiali Backend Project-Setup

---
+++ src/main/java/ch/zhaw/bauersteine/BauersteineApplication.java
@@ -0,0 +1,13 @@
+package ch.zhaw.bauersteine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BauersteineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BauersteineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/zhaw/bauersteine/BauersteineApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.zhaw.bauersteine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BauersteineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
