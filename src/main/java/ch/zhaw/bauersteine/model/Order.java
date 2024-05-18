081fdd63edc8215c246a95e42bd8808a73dba41e
bauerfel <bauerfel@students.zhaw.ch>
2024-05-18 12:49:17 +0200

    refactor: Update static resource locations in application.properties

---
+++ src/main/java/ch/zhaw/bauersteine/model/Order.java
@@ -0,0 +1,9 @@
+package ch.zhaw.bauersteine.model;
+
+import org.springframework.data.annotation.Id;
+
+public class Order {
+    
+     @Id
+    private String id;
+}
---
+++ src/main/resources/static/_app/immutable/entry/start.C6LvCowr.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.BKcOnAlx.js";export{t as start};
